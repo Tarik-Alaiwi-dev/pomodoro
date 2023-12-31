59214626220ef39ec68074ae95fbdcaf855cc8e8
Tarik-Alaiwi-dev <153948308+Tarik-Alaiwi-dev@users.noreply.github.com>
2023-12-31 19:01:49 +0100

    added timer

--- pomodoro/scripts.js
+++ pomodoro/scripts.js
@@ -1,8 +1,19 @@
 //get placeholders
-let timer = document.querySelector(".timer").innerHTML;
+let timer = document.querySelector(".timer");
+let title = document.getElementById("title");
 
-//Start function
-function start(){
-    const time = setInterval(function(){
-    }, 1000);
+let startingTime = 25*60;
+
+function odliczanie(){
+    let minuty = Math.floor(startingTime/60);
+    let seconds = startingTime%60;
+
+    timer.innerHTML = minuty + ':' + (seconds<10 ? '0' : '') + seconds;
+    title.innerHTML = minuty + ':' + (seconds<10 ? '0' : '') + seconds + " - Time to focus!";
+
+    if(startingTime === 0){
+        clearInterval(odliczanieInterwal);
+    }else{
+        startingTime--;
+    }
 }
