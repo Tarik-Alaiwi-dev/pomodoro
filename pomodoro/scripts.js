068be06688dccb9401ebdbb260ce1d74db599d9d
Tarik-Alaiwi-dev <153948308+Tarik-Alaiwi-dev@users.noreply.github.com>
2024-01-07 17:09:23 +0100

    fixed timer, start/pause

--- pomodoro/scripts.js
+++ pomodoro/scripts.js
@@ -1,21 +1,37 @@
-//get placeholders
-let timer = document.querySelector(".timer");
-let title = document.getElementById("title");
+let timer;
+let minutes = 25;
+let seconds = 0;
+let tabTitle = document.getElementById('title');
 
-let startingTime = 25*60;
+function startTimer() {
+  timer = setInterval(updateTimer, 1000);
+  document.querySelector('.start').classList.add('start-hide');
+  document.querySelector('.pause').classList.add('pause-display');
+}
 
-let pause = 0;
+function stopTimer() {
+  clearInterval(timer);
+  document.querySelector('.start').classList.remove('start-hide');
+  document.querySelector('.pause').classList.remove('pause-display');
+}
 
-function odliczanie(){
-    let minuty = Math.floor(startingTime/60);
-    let seconds = startingTime%60;
-
-    timer.innerHTML = minuty + ':' + (seconds<10 ? '0' : '') + seconds;
-    title.innerHTML = minuty + ':' + (seconds<10 ? '0' : '') + seconds + " - Time to focus!";
-
-    if(startingTime === 0 || pause === 1){
-        clearInterval(odliczanieInterwal);
-    }else{
-        startingTime--;
+function updateTimer() {
+  if (minutes === 0 && seconds === 0) {
+    stopTimer();
+    alert("Czas minął!");
+  } else {
+    if (seconds === 0) {
+      minutes--;
+      seconds = 59;
+    } else {
+      seconds--;
     }
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    const timeDisplay = `${formattedMinutes}:${formattedSeconds}`;
+
+    document.querySelector('.timer').innerText = timeDisplay;
+    document.getElementById('title').innerHTML = timeDisplay + ' - Time to focus!';
+  }
 }
